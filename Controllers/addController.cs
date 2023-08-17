64229e722a0abdfebbaa716f66bb33bffc4b48c7
unknown <sohrabloee.b@gmail.com>
2023-08-17 19:07:36 +0800

    sol01

---
+++ Controllers/addController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sol01.Controllers
+{
+    public class addController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public Int16 add(Int16 N1, Int16 N2)
+        {
+            Int16 result = (short)(N1 + N2);
+            return result;
+        }
+    }
+}
